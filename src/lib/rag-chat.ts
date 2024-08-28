918e010bb6cb37ab3d1cf7fa4754fe46d018fce2
rozer <shubhamsinha07084@gmail.com>
2024-08-28 23:13:59 +0530

    almost done

--- src/lib/rag-chat.ts
+++ src/lib/rag-chat.ts
@@ -1,8 +1,10 @@
 import { RAGChat, upstash } from "@upstash/rag-chat";
+import { redis } from "./redis";
 
 export const ragChat = new RAGChat({
   // using upstash model - an openai alternative
   model: upstash("meta-llama/Meta-Llama-3-8B-Instruct"),
+  redis: redis,
 });
 
 // remember, this model expexts an api key
